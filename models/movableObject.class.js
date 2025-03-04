73a754b9322b6d658bea6809250ecbe78c37e6b3
Aylin <a.celikhan@outlook.de>
2025-03-04 22:10:25 +0100

    refactoring and adding frames for collision detection

--- models/movableObject.class.js
+++ models/movableObject.class.js
@@ -8,6 +8,18 @@ class MovableObject {
     acceleration = 5;
     otherDirection = false;
 
+    draw(ctx) {
+        ctx.drawImage(this.img, this.x, this.y, this.width, this.height);
+    }
+
+    drawFrame(ctx) {
+        ctx.beginPath();
+        ctx.lineWidth = '5';
+        ctx.strokeStyle = 'blue';
+        ctx.rect(this.x, this.y, this.width, this.height);
+        ctx.stroke();
+    }
+
     applyGravity() {
         setInterval(() => {
             if (this.isAboveGround() || this.speedY > 0) {
