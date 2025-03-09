f5743f192cc894acdfab19cbaa64cc4ff2a14436
Aylin <a.celikhan@outlook.de>
2025-03-09 18:53:21 +0100

    added morge clouds with a different clouds picture and random speed

--- models/cloud.class.js
+++ models/cloud.class.js
@@ -1,16 +1,31 @@
 class Cloud extends MovableObject {
+    speed;
     y = 0;
     height = 400;
     width = 700;
-    speed = 0.15;
+    images = [
+        'img/5_background/layers/4_clouds/1.png',
+        'img/5_background/layers/4_clouds/2.png'
+    ];
 
     constructor() {
-        super().loadImage('img/5_background/layers/4_clouds/1.png');
-        this.x = Math.random() * 500;
-        this. animate();
+        super();
+        this.loadImage(this.getRandomImage());
+        this.x = Math.random() * 10000;
+        this.speed = 0.1 + Math.random() * 0.4;
+        this.animate();
+    }
+
+    getRandomImage() {
+        return this.images[Math.floor(Math.random() * this.images.length)];
     }
 
     animate() {
-        this.moveLeft();
+        setInterval(() => {
+            this.x -= this.speed;
+            if (this.x + this.width < 0) {
+                this.x = 7000;
+            }
+        }, 1000 / 60);
     }
 }
