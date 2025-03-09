90d7ff7c5a3f8b1ab0c1b2d45b0f47aea002b3ab
Aylin <a.celikhan@outlook.de>
2025-03-09 19:23:58 +0100

    added bottle rotation when throwing bottle

--- models/ThrowableObject.class.js
+++ models/ThrowableObject.class.js
@@ -1,6 +1,24 @@
 class ThrowableObject extends MovableObject {
+    imagesRotation = [
+        'img/6_salsa_bottle/bottle_rotation/1_bottle_rotation.png',
+        'img/6_salsa_bottle/bottle_rotation/2_bottle_rotation.png',
+        'img/6_salsa_bottle/bottle_rotation/3_bottle_rotation.png',
+        'img/6_salsa_bottle/bottle_rotation/4_bottle_rotation.png'
+    ];
+    imagesSplash = [
+        'img/6_salsa_bottle/bottle_rotation/bottle_splash/1_bottle_splash.png',
+        'img/6_salsa_bottle/bottle_rotation/bottle_splash/2_bottle_splash.png',
+        'img/6_salsa_bottle/bottle_rotation/bottle_splash/3_bottle_splash.png',
+        'img/6_salsa_bottle/bottle_rotation/bottle_splash/4_bottle_splash.png',
+        'img/6_salsa_bottle/bottle_rotation/bottle_splash/5_bottle_splash.png',
+        'img/6_salsa_bottle/bottle_rotation/bottle_splash/6_bottle_splash.png',
+    ];
+
     constructor(x, y) {
-        super().loadImage('img/6_salsa_bottle/salsa_bottle.png');
+        super();
+        this.loadImage(this.imagesRotation[0]);
+        this.loadImages(this.imagesRotation);
+        this.loadImages(this.imagesSplash);
         this.x = x;
         this.y = y;
         this.height = 80;
@@ -9,10 +27,18 @@ class ThrowableObject extends MovableObject {
     }
 
     throw() {
-        this.speedY = 40;
+        this.speedY = 25;
         this.applyGravity();
-        setInterval(() => {
+        let throwInterval = setInterval(() => {
             this.x += 10;
         }, 25);
+
+        this.animateRotation();
+    }
+
+    animateRotation() {
+        setInterval(() => {
+            this.playAnimation(this.imagesRotation);
+        }, 50);
     }
 }
